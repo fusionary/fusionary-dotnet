b346b92c8d5d2930602512e16cccbd7a886fb956
Jeremy Bell <jeremy.bell@fusionary.com>
2022-08-02 13:50:43 -0400

    Use env to get Application Name

--- src/Fusionary.OpenApi/ConfigureOptions/ConfigureSwaggerUIOptions.cs
+++ src/Fusionary.OpenApi/ConfigureOptions/ConfigureSwaggerUIOptions.cs
@@ -1,6 +1,5 @@
-using Fusionary.Core;
-
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Options;
 
@@ -9,34 +8,41 @@ using Swashbuckle.AspNetCore.SwaggerUI;
 namespace Fusionary.OpenApi.ConfigureOptions;
 
 /// <summary>
-/// Configure Swagger UI options.
+///     Configure Swagger UI options.
 /// </summary>
 public class ConfigureSwaggerUIOptions : IConfigureOptions<SwaggerUIOptions> {
     private readonly IApiVersionDescriptionProvider apiVersionDescriptionProvider;
+    private readonly IWebHostEnvironment env;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ConfigureSwaggerUIOptions" /> class.
+    ///     Initializes a new instance of the <see cref="ConfigureSwaggerUIOptions" /> class.
     /// </summary>
     /// <param name="apiVersionDescriptionProvider">Api version description provider.</param>
-    public ConfigureSwaggerUIOptions(IApiVersionDescriptionProvider apiVersionDescriptionProvider) {
+    /// <param name="env">Environment information</param>
+    public ConfigureSwaggerUIOptions(
+        IApiVersionDescriptionProvider apiVersionDescriptionProvider,
+        IWebHostEnvironment env
+    )
+    {
         this.apiVersionDescriptionProvider = apiVersionDescriptionProvider;
+        this.env = env;
     }
 
 
     /// <inheritdoc />
-    public void Configure(SwaggerUIOptions options) {
-        options.DocumentTitle = AssemblyInformation.Current.Product;
+    public void Configure(SwaggerUIOptions options)
+    {
+        options.DocumentTitle = env.ApplicationName;
 
         options.DisplayOperationId();
         options.DisplayRequestDuration();
 
         foreach (var apiVersionDescription in apiVersionDescriptionProvider
                      .ApiVersionDescriptions
-                     .OrderByDescending(x => x.ApiVersion)) {
+                     .OrderByDescending(x => x.ApiVersion))
             options.SwaggerEndpoint(
                 $"/swagger/{apiVersionDescription.GroupName}/swagger.json",
                 $"Version {apiVersionDescription.ApiVersion}"
             );
-        }
     }
 }
